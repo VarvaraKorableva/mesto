50c5b915ce2ee64df1247751f01c2c06afdcf22e
Varvara Korableva <v.korableva1990@yandex.ru>
2021-12-28 21:30:34 +0300

    full work

---
+++ index.js
@@ -0,0 +1,35 @@
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+let openPopup = document.querySelector('.popup_opened');
+let formElement = document.querySelector('.popup__form');
+let submitButton = document.querySelector('.popup__button');
+
+let profileName = document.getElementById('profile__name');
+let profileJob = document.getElementById('profile__job');
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('job');
+
+
+function showPopup() {
+  popup.classList = 'popup_opened';
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+profileEditButton.addEventListener('click',showPopup);
+
+
+function closePopup() {
+  popup.classList = 'popup';
+}
+popupCloseButton.addEventListener('click',closePopup);
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popup.classList.remove ('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
